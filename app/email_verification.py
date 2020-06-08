1186576c9beddc6ed4ca796a5bb72a1fdb1a05cb
mzmpy <2316687327@qq.com>
2020-06-08 15:43:20 +0800

    移除异步发送 email 功能

--- app/email_verification.py
+++ app/email_verification.py
@@ -6,15 +6,18 @@ from threading import Thread
 from app import app
 from flask import render_template
 
-def send_async_email(app, msg):
-    with app.app_context():
-        mail.send(msg)
+# def send_async_email(app, msg):
+#     with app.app_context():
+#         mail.send(msg)
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    Thread(target=send_async_email, args=(app, msg)).start()
+    # 在 pythonanywhere 上无法使用多线程
+    # Thread(target=send_async_email, args=(app, msg)).start()
+    with app.app_context():
+        mail.send(msg)
 
 def send_register_verification_email(user):
     token = user.get_register_verification_token()
